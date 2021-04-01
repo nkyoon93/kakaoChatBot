b38fc7e9c76c2d02440e471cd1cb338e05f69fc3
yoon <nkyoon93@korea.ac.kr>
2021-04-01 23:57:54 +0900

    0401 new kakao api project

--- kakao/kakao/urls.py
+++ kakao/kakao/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^kakao/', include('kakao.urls')),
+    url('admin/', admin.site.urls),
 ]
